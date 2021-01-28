334e31582cb891dee5f7841c042f0f68534c8088
sj1218305 <jainshobit0@gmail.com>
2021-01-28 16:25:26 +0530

    set up db config

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 
 const SERVER_PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/design-days';
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = require('http').createServer(app);
 
 app.get('/',(req,res,next)=>{
-    console.log('logging in on the home route');
     res.send("Hello world");
 });
 
+mongoose.connect(MONGO_URI,{useNewUrlParser:true})
+.then(result=>{
+    console.log('connected to db');
+})
+.catch(err=>{
+    console.log(err);
+});
 
-server.listen(SERVER_PORT,()=>{console.log(`Server running on port ${SERVER_PORT}`)});
+app.listen(SERVER_PORT,()=>{console.log(`Server running on port ${SERVER_PORT}`)});
